97f1c4d4fbbdbde58a5c37b657ce8ac0b51f1b15
Trung Hoàng <xinso.dev@gmail.com>
2024-07-24 00:31:26 +0700

    Add marriage migration

---
+++ src/Persistence/EntityFramework/Migrations/20240723172209_Marriage.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="20240723172209_Marriage.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+#nullable disable
+
+namespace MUnique.OpenMU.Persistence.EntityFramework.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class Marriage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marriage",
+                schema: "data",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CharacterId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PartnerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MarriedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marriage", x => x.Id);
+                    table.UniqueConstraint("AK_Marriage_CharacterId", x => new { x.CharacterId });
+                    table.UniqueConstraint("AK_Marriage_PartnerId", x => new { x.PartnerId });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marriage",
+                schema: "data");
+        }
+    }
+}
